1373e14e515dc0270fc2aeb632d3908f1c1fbe18
Del Reid <45520351+itsjustdel@users.noreply.github.com>
2022-03-20 17:42:21 +0000

    message edit for multiple instances

--- Il2ServerAppLateNight/Il2ServerAppLateNight/WinformsProject.cpp
+++ Il2ServerAppLateNight/Il2ServerAppLateNight/WinformsProject.cpp
@@ -13,15 +13,14 @@ int main() {
         Application::EnableVisualStyles();
         Application::SetCompatibleTextRenderingDefault(false);
         Application::Run(gcnew Il2Dials::Form1());
+        //Garbage collection will get rid of mutex reference if we don't use this line
+        GC::KeepAlive(mutex);
     }
     else {
 
-        MessageBox::Show("IL-2 Dials Server.exe already running!");
+        MessageBox::Show("IL-2 Dials Server already running!");
        
     }
 
-	//Application::EnableVisualStyles();
-	//Application::SetCompatibleTextRenderingDefault(false);
-	//Application::Run(gcnew Il2Dials::Form1()); 
 	return 0;
 }
