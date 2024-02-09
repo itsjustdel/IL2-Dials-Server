73a104e6431bf0e26074dc8ae3bffbb86b573db5
itsjustdel <derek@circlein.xyz>
2024-02-09 11:41:28 +0000

    0.65

--- Il2ServerAppLateNight/Il2ServerAppLateNight/src/Main/Main.cpp
+++ Il2ServerAppLateNight/Il2ServerAppLateNight/src/Main/Main.cpp
@@ -33,7 +33,7 @@
 #include "../CarbMixTemp/CarbMixTemp.h"
 #include "../GERPlanes/GERPlanes.h"
 
-float version = 0.64f;
+float version = 0.65f;
 
 //how much memory to change permissions on in original code
 const int size = 100; //note, min size?
